b07dc6daf3e56b4564ed5be697bfdb6b60652c08
Yu Chuan <165673712+yc82628@users.noreply.github.com>
2026-01-03 15:36:29 +0100

    Implement background music and debug message on start
    Some missing segments of the code

--- SpdRemGameMode.cpp
+++ SpdRemGameMode.cpp
@@ -1,6 +1,8 @@
 #include "SpdRemGameMode.h"
 #include "Kismet/GameplayStatics.h"
 #include "GridCharacter.h"
+#include "Engine/World.h"
+#include "Sound/SoundBase.h"
 
 SpdRemGameMode::SpdRemGameMode()
 {
@@ -19,9 +21,46 @@ void SpdRemGameMode::StartPlay()
 	CurrentScore = 0;
 	bIsGameOver = false;
 
+	if (BackgroundMusic)
+	{
+		// 0.0f volume multiplier, 1.0f pitch, 0.0f start time, looping by default in cue/asset
+		UGameplayStatics::PlaySound2D(this, BackgroundMusic);
+	}
+
 	if (GEngine)
 	{
-		GEngine->Ad
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Speedy Rempit Game Start!")
 	}
 }
+
+void SpdRemGameMode::AddScore(int32 Points)
+{
+	if (!bIsGameOver)
+	{
+		return;
+	}
+
+	CurrentScore += Points;
+	if (CurrentScore > HighScore)
+	{
+		HighScore = CurrentScore;
+	}
+}
+
+void SpdRemGameMode::GameOver()
+{
+	bIsGameOver = true;
+
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("Game Over! Final Score: %d"), Current Score);
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, Message);
+	}
+}
+
+void SpdRemGameMode::RestartGame()
+{
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+}
+
 
